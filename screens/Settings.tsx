9d511cd798531e82858a9dffcd99534a3561d657
xenedium <ahmed@abderraziq.com>
2022-07-14 15:27:45 +0100

    Added api call in settings

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, SafeAreaView, Image } from 'react-native'
-import { NavigationProps } from '../types';
+import { NavigationProps, User } from '../types';
+import * as FileSystem from 'expo-file-system';
+import * as SecureStore from 'expo-secure-store';
+import { API_URL } from '../constants'
 
 type Props = NavigationProps<"Settings">;
 
 export default function Settings({ navigation, route }: Props) {
+
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        SecureStore.getItemAsync("token").then(token => {
+            if (!token) {
+                navigation.navigate("Login");
+            }
+            else {
+                fetch(`${API_URL}/api/users/me`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `JWT ${token}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(res => setUser(res.user))
+                    .catch(err => console.log(err))
+            }
+
+        })
+    }, [])
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Settings</Text>
+            {
+                !user ? null :
+                    <Text> {user.email} </Text>
+            }
         </SafeAreaView>
     )
 }
